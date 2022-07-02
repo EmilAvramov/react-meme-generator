398476b99fcb5742d75cc29f9459ac70673df329
EmilAvramov <e_avramov@zoho.eu>
2022-07-02 23:18:18 +0300

    added random data to test functionality and added state to form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,25 @@
+import { useState } from 'react';
+import { memes } from '../memesData';
+
 export default function Form() {
+	const [mime, setMime] = useState({
+		top: '',
+		bottom: '',
+		img: '',
+	});
+
+	function getMimeData() {
+		const [topContent, bottomContent] = document.querySelectorAll('input');
+		const random = parseInt(Math.random() * 100);
+		const img = memes.data.memes[random].url;
+
+		setMime({
+			top: topContent.value,
+			bottom: bottomContent.value,
+			img: img,
+		});
+	}
+
 	return (
 		<main>
 			<form className='form' action=''>
@@ -19,8 +40,19 @@ export default function Form() {
 					/>
 				</div>
 
-				<button type='submit'>Get a new meme image 🖼</button>
+				<button
+					onClick={(e) => {
+						e.preventDefault();
+						getMimeData();
+					}}
+					type='submit'
+				>
+					Get a new meme image 🖼
+				</button>
 			</form>
+			<div className='mime-img'>
+				<img src={mime.img} alt='' />
+			</div>
 		</main>
 	);
 }
